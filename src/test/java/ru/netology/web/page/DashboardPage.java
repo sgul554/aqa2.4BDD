b00eff071a0a9f4417766fbbf416a1f26cd06a18
Sasha Gulyaeva <sgulayeva@gmail.com>
2021-12-15 00:54:31 +0300

    add test1

--- src/test/java/ru/netology/web/page/DashboardPage.java
+++ src/test/java/ru/netology/web/page/DashboardPage.java
@@ -15,8 +15,8 @@ public class DashboardPage {
     private final String balanceFinish = " р.";
 
     private SelenideElement headingPage = $("[data-test-id=dashboard]");
-    private SelenideElement buttonFirstCard = $("[data-test-id=92df3f1c-a033-48e6-8390-206f6b1f56c0] button");
-    private SelenideElement buttonSecondCard = $("[data-test-id=0f3f5c2a-249e-4c3d-8287-09f7a039391d] button");
+    private SelenideElement buttonFirstCard = $("[data-test-id='92df3f1c-a033-48e6-8390-206f6b1f56c0'] button");
+    private SelenideElement buttonSecondCard = $("[data-test-id='0f3f5c2a-249e-4c3d-8287-09f7a039391d'] button");
 
     public DashboardPage() {
         headingPage.shouldBe(Condition.visible);
--- src/test/java/ru/netology/web/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/web/test/MoneyTransferTest.java
@@ -1,4 +1,30 @@
 package ru.netology.web.test;
 
+import lombok.val;
+import org.junit.jupiter.api.Test;
+import ru.netology.web.data.DataHelper;
+import ru.netology.web.page.LoginPage;
+
+import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MoneyTransferTest {
+    @Test
+    void shouldTransferZeroFromFirstCard (){
+        val loginPage = open("http://localhost:9999", LoginPage.class);
+        val authInfo = DataHelper.getAuthInfo();
+        val verificationPage = loginPage.validLogin(authInfo);
+        val verificationCode = DataHelper.getVerificationCodeFor(authInfo);
+        val dashboardPage = verificationPage.validVerify(verificationCode);
+        val balanceFirstCardBefore = dashboardPage.getFirstCardBalance();
+        val balanceSecondCardBefore = dashboardPage.getSecondCardBalance();
+        val transferPage = dashboardPage.secondCardDeposit();
+        int amount = 0;
+        transferPage.transferMoney(amount,DataHelper.getFirstCardNumber());
+        val balanceFirstCardAfter = dashboardPage.getFirstCardBalance();
+        val balanceSecondCardAfter = dashboardPage.getSecondCardBalance();
+        assertEquals((balanceFirstCardBefore - amount), balanceFirstCardAfter);
+        assertEquals((balanceSecondCardBefore + amount), balanceSecondCardAfter);
+    }
+
 }
